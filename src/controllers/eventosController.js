4238beb4a25af364352ec4bb5ca5a253f497488c
julian2238 <72612081+julian2238@users.noreply.github.com>
2025-06-15 21:48:05 -0500

    Creando endpoints get y post de eventos

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -1,24 +1,162 @@
+const { db, admin } = require("../firebase");
 
-const getEventos = (req, res) => {
-    try {
-        
-    } catch (error) {
-        res.status(500).send(error.message || 'Internal server error');
+const getEventos = async (req, res) => {
+  try {
+    const eventosSnap = await db.collection("eventos").get();
+
+    if (eventosSnap.empty) {
+      return res.status(200).send({
+        status: true,
+        message: "No hay eventos registrados",
+        eventos: [],
+      });
     }
+
+    const eventos = [];
+    const tpEventoPromises = [];
+
+    eventosSnap.forEach((doc) => {
+      const eventoData = doc.data();
+      const eventoId = doc.id;
+      const tpEventoRef = eventoData.tpEvento;
+
+      let fechaEvento = null;
+      let horaEvento = null;
+
+      if (
+        eventoData.dtEvento &&
+        eventoData.dtEvento instanceof admin.firestore.Timestamp
+      ) {
+        const dateObj = eventoData.dtEvento.toDate();
+
+        fechaEvento = dateObj.toLocaleDateString("es-CO", {
+          year: "numeric",
+          month: "2-digit",
+          day: "2-digit",
+        });
+
+        horaEvento = dateObj.toLocaleTimeString("es-CO", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        });
+      }
+
+      delete eventoData.dtEvento;
+      delete eventoData.dtCreacion;
+
+      eventos.push({
+        id: eventoId,
+        tpEvento: "",
+        fechaEvento,
+        horaEvento,
+        ...eventoData,
+      });
+
+      if (tpEventoRef && tpEventoRef.path) {
+        tpEventoPromises.push(tpEventoRef.get());
+      } else {
+        tpEventoPromises.push(Promise.resolve(null));
+      }
+    });
+
+    const tpEventoSnaps = await Promise.all(tpEventoPromises);
+
+    const result = eventos.map((evento, index) => {
+      const tpEventoSnap = tpEventoSnaps[index];
+
+      if (tpEventoSnap && tpEventoSnap.exists) {
+        return {
+          ...evento,
+          tpEvento: tpEventoSnap.data().name,
+        };
+      }
+      return evento;
+    });
+
+    res.status(200).send({
+      status: true,
+      message: "Eventos obtenidos correctamente",
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: false,
+      message: error.message || "Internal server error",
+    });
+  }
 };
 
 const getEventosById = (req, res) => {};
 
-const postEventos = (req, res) => {};
+const postEventos = async (req, res) => {
+  try {
+    const data = req.body;
+
+    validarCampos = (data) => {
+      const requiredFields = [
+        "tpEvento",
+        "nmEvento",
+        "date",
+        "hour",
+        "organizador",
+        "ruta",
+      ];
+      return requiredFields.every((field) => data.hasOwnProperty(field));
+    };
+
+    if (!validarCampos(data)) {
+      return res.status(400).send({
+        status: false,
+        message: "Faltan campos requeridos",
+      });
+    }
+
+    const tpEventoRef = db.collection("tipo_evento").doc(data.tpEvento);
+    const tpEventoSnaop = await tpEventoRef.get();
+
+    if (!tpEventoSnaop.exists) {
+      return res.status(400).send({
+        status: false,
+        message: "Tipo de evento no existe",
+      });
+    }
+
+    const newEvent = {
+      tpEvento: tpEventoRef,
+      nmEvento: data.nmEvento,
+      organizadorEvento: data.organizador,
+      rutaEvento: data.ruta,
+      participantes: [],
+      dtEvento: admin.firestore.Timestamp.fromDate(
+        new Date(`${data.date}T${data.hour}:00`)
+      ),
+      dtCreacion: admin.firestore.Timestamp.now(),
+    };
+
+    const docRef = db.collection("eventos").add(newEvent);
+
+    res.status(200).send({
+      status: true,
+      message: "Evento creado correctamente",
+      data: docRef.id,
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: false,
+      message: error.message || "Internal server error",
+    });
+  }
+};
 
 const putEventos = (req, res) => {};
 
 const deleteEventos = (req, res) => {};
 
 module.exports = {
-    getEventos,
-    getEventosById,
-    postEventos,
-    putEventos,
-    deleteEventos
-}
+  getEventos,
+  getEventosById,
+  postEventos,
+  putEventos,
+  deleteEventos,
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 // app.use(require('./routes/index'));
@@ -17,5 +17,4 @@ app.use("/api/v1/eventos", require("./routes/eventos"));
 
 app.listen(4000);
 
-console.log('Server running on port 4000');
-
+console.log("Server running on port 4000");
